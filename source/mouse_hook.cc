4d441c16930548f729ebb4005c2a2e36dc1d2fcc
Sutthinart khunvadhana <sutthinart.khunvadhana@thomsonreuters.com>
2018-09-14 15:09:05 +0700

    Fix triggering modal dialog warning in other apps

--- source/mouse_hook.cc
+++ source/mouse_hook.cc
@@ -7,173 +7,200 @@
 using namespace std;
 
 HHOOK lowLevelMouseHook = NULL;
-HHOOK CBTHook = NULL;
+
+HWINEVENTHOOK g_hook = NULL;
 
 HINSTANCE dllInstance = NULL;
 
-static HANDLE MouseThread;
-static HANDLE CBTThread;
-
 LRESULT CALLBACK LowLevelMouseProc(int, WPARAM, LPARAM);
-LRESULT CALLBACK CBTProc (int, WPARAM, LPARAM);
+
+void CALLBACK HandleWinEvent(HWINEVENTHOOK, DWORD, HWND, LONG, LONG, DWORD, DWORD);
 
 void RunThread(void* arg) {
-	MouseHookManager* mouse = (MouseHookManager*) arg;
-	mouse->_Run();
+  MouseHookManager* mouse = (MouseHookManager*) arg;
+  mouse->_Run();
 }
 
 MouseHookRef MouseHookRegister(MouseHookCallback callback, void* data) {
-	return MouseHookManager::GetInstance()->Register(callback, data);
+  return MouseHookManager::GetInstance()->Register(callback, data);
 }
 
 void MouseHookUnregister(MouseHookRef ref) {
-	MouseHookManager::GetInstance()->Unregister(ref);
+  MouseHookManager::GetInstance()->Unregister(ref);
+}
+
+void Unhook() {
+  if (lowLevelMouseHook != NULL) {
+    UnhookWindowsHookEx(lowLevelMouseHook);
+    lowLevelMouseHook = NULL;
+  }
+
+  if (g_hook != NULL) {
+    UnhookWinEvent(g_hook);
+    g_hook = NULL;
+  }
 }
 
 MouseHookManager* MouseHookManager::GetInstance() {
-	static MouseHookManager manager;
-	return &manager;
+  static MouseHookManager manager;
+  return &manager;
 }
 
 MouseHookManager::MouseHookManager() {
-	running = false;
-	pause = false;
-	thread_id = NULL;
+  running = false;
+  pause = false;
+  thread_id = NULL;
 
-	listeners = new std::list<MouseHookRef>();
-	uv_mutex_init(&event_lock);
-	uv_mutex_init(&init_lock);
-	uv_cond_init(&init_cond);
+  listeners = new std::list<MouseHookRef>();
+  uv_mutex_init(&event_lock);
+  uv_mutex_init(&init_lock);
+  uv_cond_init(&init_cond);
 }
 
 MouseHookManager::~MouseHookManager() {
-	if(!listeners->empty()) Stop();
+  if(!listeners->empty()) Stop();
 
-	delete listeners;
-	uv_mutex_destroy(&event_lock);
-	uv_mutex_destroy(&init_lock);
-	uv_cond_destroy(&init_cond);
+  delete listeners;
+  uv_mutex_destroy(&event_lock);
+  uv_mutex_destroy(&init_lock);
+  uv_cond_destroy(&init_cond);
 }
 
 MouseHookRef MouseHookManager::Register(MouseHookCallback callback, void* data) {
-	uv_mutex_lock(&event_lock);
-	
-	bool empty = listeners->empty();
+  uv_mutex_lock(&event_lock);
+  
+  bool empty = listeners->empty();
 
-	MouseHookRef entry = new MouseHookEntry();
-	entry->callback = callback;
-	entry->data = data;
-	listeners->push_back(entry);
+  MouseHookRef entry = new MouseHookEntry();
+  entry->callback = callback;
+  entry->data = data;
+  listeners->push_back(entry);
 
-	uv_mutex_unlock(&event_lock);	
+  uv_mutex_unlock(&event_lock);	
 
   if(empty) uv_thread_create(&thread, RunThread, this);
 
-	return entry;
+  return entry;
 }
 
 void MouseHookManager::Unregister(MouseHookRef ref) {
-	uv_mutex_lock(&event_lock);
+  uv_mutex_lock(&event_lock);
 
-	listeners->remove(ref);
-	delete ref;
-	bool empty = listeners->empty();
+  listeners->remove(ref);
+  delete ref;
+  bool empty = listeners->empty();
 
-	uv_mutex_unlock(&event_lock);
-	
-	if(empty) Stop();
+  uv_mutex_unlock(&event_lock);
+  
+  if(empty) Stop();
 }
 
 void MouseHookManager::_HandleEvent(WPARAM type, POINT point) {
-	if (!pause) {
-		uv_mutex_lock(&event_lock);
+  uv_mutex_lock(&event_lock);
 
-		for(std::list<MouseHookRef>::iterator it = listeners->begin(); it != listeners->end(); it++) {
-			(*it)->callback(type, point, (*it)->data);
-		}
+  for(std::list<MouseHookRef>::iterator it = listeners->begin(); it != listeners->end(); it++) {
+    (*it)->callback(type, point, (*it)->data);
+  }
 
-		uv_mutex_unlock(&event_lock);
-	}
+  uv_mutex_unlock(&event_lock);
 }
 
 void MouseHookManager::_Run() {
   MSG msg;
-	BOOL val;
+  BOOL val;
 
-	uv_mutex_lock(&init_lock);
+  uv_mutex_lock(&init_lock);
 
-	PeekMessage(&msg, NULL, WM_USER, WM_USER, PM_NOREMOVE);
-	thread_id = GetCurrentThreadId();
-	
-	uv_cond_signal(&init_cond);
-	uv_mutex_unlock(&init_lock);
+  PeekMessage(&msg, NULL, WM_USER, WM_USER, PM_NOREMOVE);
+  thread_id = GetCurrentThreadId();
+  
+  uv_cond_signal(&init_cond);
+  uv_mutex_unlock(&init_lock);
 
-	lowLevelMouseHook = SetWindowsHookEx(WH_MOUSE_LL, LowLevelMouseProc, (HINSTANCE) NULL, 0);
-  CBTHook = SetWindowsHookEx(WH_CBT, CBTProc, dllInstance, 0);
+  lowLevelMouseHook = SetWindowsHookEx(WH_MOUSE_LL, LowLevelMouseProc, (HINSTANCE) NULL, 0);
 
-	while((val = GetMessage(&msg, NULL, 0, 0)) != 0) {
-		if(val == -1) throw std::runtime_error("GetMessage failed (return value -1)");
-		if(msg.message == WM_STOP_MESSAGE_LOOP) break;
-	}
+  g_hook = SetWinEventHook(
+    EVENT_SYSTEM_MENUSTART, EVENT_SYSTEM_MENUEND,  // Range of events (4 to 5).
+    NULL,                                          // Handle to DLL.
+    HandleWinEvent,                                // The callback.
+    0, 0,              // Process and thread IDs of interest (0 = all)
+    WINEVENT_OUTOFCONTEXT); // Flags.
 
-	UnhookWindowsHookEx(lowLevelMouseHook);
-  UnhookWindowsHookEx(CBTHook);
+  while((val = GetMessage(&msg, NULL, 0, 0)) != 0) {
+    if(val == -1) throw std::runtime_error("GetMessage failed (return value -1)");
+    if(msg.message == WM_STOP_MESSAGE_LOOP) break;
+  }
 
-	uv_mutex_lock(&init_lock);
-	thread_id = NULL;
-	uv_mutex_unlock(&init_lock);
+  UnhookWindowsHookEx(lowLevelMouseHook);
+  UnhookWinEvent(g_hook);
+
+  uv_mutex_lock(&init_lock);
+  thread_id = NULL;
+  uv_mutex_unlock(&init_lock);
 }
 
 void MouseHookManager::_HandlePause(bool value) {
-	pause = value;
+  pause = value;
 }
 
 void MouseHookManager::Stop() {
-	uv_mutex_lock(&init_lock);
+  uv_mutex_lock(&init_lock);
 
-	while(thread_id == NULL) uv_cond_wait(&init_cond, &init_lock);
-	DWORD id = thread_id;
-	
-	uv_mutex_unlock(&init_lock);
+  while(thread_id == NULL) uv_cond_wait(&init_cond, &init_lock);
+  DWORD id = thread_id;
+  
+  uv_mutex_unlock(&init_lock);
 
-	PostThreadMessage(id, WM_STOP_MESSAGE_LOOP, NULL, NULL);
-	uv_thread_join(&thread);
-}
-
-BOOL APIENTRY DllMain(HMODULE module, DWORD reasonForCall, LPVOID reserved) {
-	dllInstance = (HINSTANCE)module;
-	return TRUE;
+  PostThreadMessage(id, WM_STOP_MESSAGE_LOOP, NULL, NULL);
+  uv_thread_join(&thread);
 }
 
 LRESULT CALLBACK LowLevelMouseProc(int nCode, WPARAM wParam, LPARAM lParam) {
-	if(nCode >= 0) {
-		MSLLHOOKSTRUCT* data = (MSLLHOOKSTRUCT*) lParam;
-		POINT point = data->pt;
+  if(nCode >= 0) {
+    MSLLHOOKSTRUCT* data = (MSLLHOOKSTRUCT*) lParam;
+    POINT point = data->pt;
 
-		MouseHookManager::GetInstance()->_HandleEvent(wParam, point);
-	}
+    MouseHookManager::GetInstance()->_HandleEvent(wParam, point);
+  }
 
-	return CallNextHookEx(lowLevelMouseHook, nCode, wParam, lParam);
+  return CallNextHookEx(NULL, nCode, wParam, lParam);
 }
 
-LRESULT CALLBACK CBTProc(int code, WPARAM wParam, LPARAM lParam) {
-  if(code == HCBT_CREATEWND) {
-    HWND hwnd = (HWND)wParam;
-    CHAR name[1024] = {0};
-    GetClassName(hwnd, name, sizeof(name));
-    string className(name);
-    // cout << ">> window class: " << className << endl;
-    if(className == "#32768") { // The class for a menu.
-      MouseHookManager::GetInstance()->_HandlePause(true);
+void CALLBACK HandleWinEvent(HWINEVENTHOOK hook, DWORD event, HWND hwnd, 
+                             LONG idObject, LONG idChild, 
+                             DWORD dwEventThread, DWORD dwmsEventTime)
+{
+    switch (event) {
+      case EVENT_SYSTEM_MENUSTART:
+        // cout << "EVENT_SYSTEM_MENUSTART >>>>>>>>>>>>>>>>>>>>>>>>" << endl;
+        if (lowLevelMouseHook != NULL) {
+          UnhookWindowsHookEx(lowLevelMouseHook);
+          lowLevelMouseHook = NULL;
+        }
+        break;
+      case EVENT_SYSTEM_MENUEND:
+        // cout << "<<<<<<<<<<<<<<<<<<<<<<<< EVENT_SYSTEM_MENUEND" << endl;
+        if (lowLevelMouseHook == NULL) {
+          lowLevelMouseHook = SetWindowsHookEx(WH_MOUSE_LL, LowLevelMouseProc, (HINSTANCE) NULL, 0);
+        }
+        break;
     }
-  } else if(code == HCBT_DESTROYWND) {
-    HWND hwnd = (HWND)wParam;
-    CHAR name[1024] = {0};
-    GetClassName(hwnd, name, sizeof(name));
-    string className(name);
-    if(className == "#32768") {
-      MouseHookManager::GetInstance()->_HandlePause(false);
-    }
+}
+
+BOOL WINAPI DllMain(HMODULE module, DWORD reasonForCall, LPVOID reserved) {
+  switch (reasonForCall) {
+    case DLL_PROCESS_ATTACH:
+      dllInstance = (HINSTANCE)module;
+      break;
+
+    case DLL_PROCESS_DETACH:
+      Unhook();
+      break;
+
+    case DLL_THREAD_ATTACH:
+    case DLL_THREAD_DETACH:
+      break;
   }
-	return CallNextHookEx(CBTHook, code, wParam, lParam);
+
+  return TRUE;
 }
